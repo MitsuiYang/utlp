9f3d4837aca3c8d06d00d7ac2a473f91f30ff45b
sora <sora@haeena.net>
2016-05-03 02:58:10 +0900

    pcie_mmio: nfsume: top moudle

---
+++ boards/nfsume/rtl/top.sv
@@ -0,0 +1,26 @@
+`include "../rtl/setup.v"
+`timescale 1ps / 1ps
+
+module top #(
+	parameter PL_LINK_CAP_MAX_LINK_WIDTH = 8
+)(
+	output wire [(PL_LINK_CAP_MAX_LINK_WIDTH - 1) : 0] pci_exp_txp,
+	output wire [(PL_LINK_CAP_MAX_LINK_WIDTH - 1) : 0] pci_exp_txn,
+	input  wire [(PL_LINK_CAP_MAX_LINK_WIDTH - 1) : 0] pci_exp_rxp,
+	input  wire [(PL_LINK_CAP_MAX_LINK_WIDTH - 1) : 0] pci_exp_rxn,
+
+	input wire sys_clk_p,
+	input wire sys_clk_n,
+	input wire sys_rst_n,
+
+	input wire clk200_p,
+	input wire clk200_n,
+
+	output wire [7:0] led
+);
+
+pcie_top pcie_top (.*);
+
+
+endmodule
+
