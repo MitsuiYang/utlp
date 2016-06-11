381892e65cbe894d8fa307e33bb53ac030a5e52d
sora <sora@haeena.net>
2016-06-11 19:41:39 +0900

    update testbench

--- boards/nfsume/test/testbench.sv
+++ boards/nfsume/test/testbench.sv
@@ -16,23 +16,35 @@ wire ETH1_TX_DISABLE;
 eth_top #(
 	.ifg_len(28'hF)
 ) eth1_top (
-	.user_clk        (user_clk),
-	.clk100          (clk100),
-	.cold_reset      (cold_reset),
-	.SFP_CLK_P       (SFP_CLK_P),
-	.SFP_CLK_N       (1'b0),
-	.SFP_REC_CLK_P   (SFP_REC_CLK_P),
-	.SFP_REC_CLK_N   (SFP_REC_CLK_N),
-	.ETH1_TX_P       (1'b0),
-	.ETH1_TX_N       (1'b0),
-	.ETH1_RX_P       (ETH1_RX_P),
-	.ETH1_RX_N       (ETH1_RX_N),
-	.I2C_FPGA_SCL    (1'b0),
-	.I2C_FPGA_SDA    (1'b0),
-	.SFP_CLK_ALARM_B (1'b0),
-	.ETH1_TX_FAULT   (1'b0),
-	.ETH1_RX_LOS     (1'b0),
-	.ETH1_TX_DISABLE (ETH1_TX_DISABLE)
+	.user_clk              (user_clk),
+	.clk100                (clk100),
+	.cold_reset            (cold_reset),
+	.SFP_CLK_P             (SFP_CLK_P),
+	.SFP_CLK_N             (1'b0),
+	.SFP_REC_CLK_P         (SFP_REC_CLK_P),
+	.SFP_REC_CLK_N         (SFP_REC_CLK_N),
+	.ETH1_TX_P             (1'b0),
+	.ETH1_TX_N             (1'b0),
+	.ETH1_RX_P             (ETH1_RX_P),
+	.ETH1_RX_N             (ETH1_RX_N),
+	.I2C_FPGA_SCL          (1'b0),
+	.I2C_FPGA_SDA          (1'b0),
+	.SFP_CLK_ALARM_B       (1'b0),
+	.ETH1_TX_FAULT         (1'b0),
+	.ETH1_RX_LOS           (1'b0),
+	.ETH1_TX_DISABLE       (ETH1_TX_DISABLE),
+	.m_axis_cq_tdata_reg   (),
+	.m_axis_cq_tuser_reg   (),
+	.m_axis_cq_tlast_reg   (),
+	.m_axis_cq_tkeep_reg   (),
+	.m_axis_cq_tvalid_reg  (),
+	.m_axis_cq_tready_reg  (),
+	.s_axis_cc_tdata_reg   (),
+	.s_axis_cc_tuser_reg   (),
+	.s_axis_cc_tlast_reg   (),
+	.s_axis_cc_tkeep_reg   (),
+	.s_axis_cc_tvalid_reg  (),
+	.s_axis_cc_tready_reg  ()
 );
-endmodule
 
+endmodule
